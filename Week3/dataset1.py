224167f835da560e5129956c5da6412eaf4a5a33
reo124 <e235441@ie.u-ryukyu.ac.jp>
2026-06-30 19:57:46 +0900

    データセットの構築

--- Week3/dataset1.py
+++ Week3/dataset1.py
@@ -1,5 +1,9 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import japanize_matplotlib
 
+# ex1.1
 def true_function(x):
     y = np.sin(np.pi * x * 0.8) * 10
     return y
@@ -7,11 +11,20 @@ def true_function(x):
 assert true_function(0) == 0
 print("The unit test was successful.")
 
-import matplotlib.pyplot as plt
-
 x = np.linspace(-1, 1, 100)
 y = true_function(x)
-
 plt.plot(x, y, label='y = sin(pi * x * 0.8) * 10')
 plt.legend()
-plt.savefig('Week3/ex1.1.png')
+plt.savefig('ex1.1.png')
+
+# ex1.2
+np.random.seed(0)
+x_obs = np.random.uniform(-1, 1, 20)
+
+y_true = true_function(x_obs)
+
+df = pd.DataFrame({"観測点" : x_obs, "真値" : y_true})
+
+plt.scatter(x_obs, y_true, label='観測点', s=100)
+plt.legend()
+plt.savefig('ex1.2.png')
